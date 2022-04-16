16fa03ce1610f8df3112f5fe4f7e8c69390bcc00
Oghenekparobo <classicsteph11@gmail.com>
2022-04-16 23:51:40 +0100

    added Read me file

--- hamburger.js
+++ hamburger.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
 btn.addEventListener('click' , () =>{ 
-    btn.classList.toggle('open');
+    // btn.classList.toggle('open');
     menu.classList.toggle('flex');
     menu.classList.toggle('hidden');
 
